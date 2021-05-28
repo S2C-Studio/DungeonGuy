2f43f0bc0002ea3bf71d5a651aa84535aca9b15f
RedHat <76946919+JudyInRedHat@users.noreply.github.com>
2021-05-28 19:52:58 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,12 +2,18 @@ function newgame(){
 	health = 100;
 	layer = 1;
 	room = 1;
+	actionPoint = 3;
 }
 function refresh(){
 	x=document.getElementById("health");
 	x.innerHTML="生命 "+health;
 	x=document.getElementById("layerAndRoom");
 	x.innerHTML="你当前在第"+layer+"层第"+room+"个房间。";
+	ap=document.getElementById("actionPoint");
+	ap.innerHTML="点击图片执行动作，当前还剩下"+actionPoint+"点行动点";
 }
-newgame()
-refresh()
+function seartch(){
+	console.log("seartch")
+}
+newgame();
+refresh();
